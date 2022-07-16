a37738dd7effc0ae5056f797fa29c931cf446736
pablokreick <pablokreick@gmail.com>
2022-07-16 10:52:56 -0300

    desafío 12 - cargando orden de compra

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
@@ -5,6 +6,28 @@ import ItemCart from "../ItemCart";
 
 const Cart = () => {
 	const { cart, totalPrice } = useCartContext();
+
+	const order = {
+		buyer: {
+			name: "Pablo",
+			email: "Pablo@gmail.com",
+			phone: "123123",
+			address: "asdd",
+		},
+		items: cart.map((product) => ({
+			id: product.id,
+			title: product.title,
+			price: product.price,
+			quantity: product.quantity,
+		})),
+		total: totalPrice(),
+	};
+
+	const handleClick = () => {
+		const db = getFirestore();
+		const ordersCollection = collection(db, "orders");
+		addDoc(ordersCollection, order).then(({ id }) => console.log(id));
+	};
 
 	if (cart.length === 0) {
 		return (
@@ -21,6 +44,7 @@ const Cart = () => {
 				<ItemCart key={product.id} product={product} />
 			))}
 			<p>total: {totalPrice()}</p>
+			<button onClick={handleClick}>Emitir compra</button>
 		</>
 	);
 };
